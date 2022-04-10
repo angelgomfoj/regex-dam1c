384529e60d75d3ccf9d933349a07b9b6cacb3a1e
J. Martin S. <jennifer.marsor@educa.jcyl.es>
2022-04-11 00:21:12 +0200

    Ejercicio06 Class created Init modified

--- regex/src/main/java/com/venancio/dam/regex/Ejercicio06.java
+++ regex/src/main/java/com/venancio/dam/regex/Ejercicio06.java
@@ -1,5 +1,112 @@
 package com.venancio.dam.regex;
 
+import java.util.regex.Pattern;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Iterator;
+
 public class Ejercicio06 {
+	
+	private StringBuilder sciText;
+	private Pattern pattern;
+	private Map<String, Integer> wordList;
+	
+	public Ejercicio06(String pat) {
+		this.sciText = new StringBuilder("texto de prueba, prueba, texto");
+		this.pattern = Pattern.compile(pat);
+		this.wordList = new HashMap<>();
+	}
+	
+	public Ejercicio06(StringBuilder input, String pat) {
+		this.sciText = input;
+		this.pattern = Pattern.compile(pat);
+		this.wordList = new HashMap<>();
+	}
+	
+	// Método para extraer todas las palabras del texto usando el patrón especificado e incluiras en el mapa
+	public Map<String, Integer> wordExtraction() {
+		
+		// Creamos un array con absolutamente todas las palabras del texto
+		String[] wordByWord = this.pattern.split(sciText);
+		
+		/* Utilizamos el HashMap para contar las ocurrencias de cada palabra:		
+		Para ello recorremos el array con todas las palabras del texto y las vamos incluyendo en el mapa
+		de modo que la clave sea la palabra (String) como tal y el valor un entero (Integer) que iremos
+		incrementando a medida que se vaya repitiendo en el texto. */
+	    for(String word : wordByWord){
+	        if(!wordList.containsKey(word)){
+	            wordList.put(word, 1);
+	        }else{
+	            wordList.put(word, wordList.get(word)+1);
+	        }
+	    }
+	    
+	    return wordList;
+	}
+	
+	/* Método que, tras determinar e indicarle cuántas veces se ha de repetir una palabra para considerarla
+	término, extrae únicamente las palabras que aparecen en el texto más veces del número especificado */
+	public void termExtraction(int numOfTimes) {
+		
+		// Mediante un iterator, eliminamos del mapa las palabras con menos repeticiones de las indicadas
+		if(wordList.isEmpty()) {
+			System.out.println("ATENCIÓN: Aún no se ha extraído ninguna palabra del texto.\nPor favor, asegúrese de extraer todas las palabras del texto antes de realizar la extracción de la terminología.");
+		} else {
+			for(Iterator<Map.Entry<String, Integer>> it = wordList.entrySet().iterator(); it.hasNext();) {
+				Map.Entry<String, Integer> word = it.next();
+				if(word.getValue() < numOfTimes) {
+					it.remove();
+				}
+			}
+			
+//			// ¡¡¡NO FUNCIONA!!!
+//			for (Map.Entry<String, Integer> word : wordList.entrySet()) {
+//				if(!wordList.isEmpty()) {
+//					if(Integer.valueOf(word.getValue()) < numOfTimes) {
+//						wordList.remove(word.getKey());
+//					}
+//				}			
+//			}
+			
+			// Mostramos la lista de términos
+			if(wordList.isEmpty()) {
+				System.out.println("ATENCIÓN: El texto no contiene ninguna palabra con el número mínimo de repeticiones especificado.");
+			} else {
+				System.out.println("\nTérmino:\tRepeticiones:");
+				wordList.forEach((word, times) -> {
+					System.out.println(word + "\t\t" + times);
+				});
+				
+//				// Otra opción
+//				for (Map.Entry<String, Integer> word : wordList.entrySet()) {
+//					System.out.print(word.getKey() + " se repite " + word.getValue() + " veces.");
+//				}
+			}
+					
+		}
+		
+	}
+	
+	// Getters y setters
+	public StringBuilder getSciText() {
+		return sciText;
+	}
+	public void setSciText(StringBuilder newText) {
+		this.sciText = newText;
+	}
+
+	public Pattern getPattern() {
+		return pattern;
+	}
+	public void setPattern(String pattern) {
+		this.pattern = Pattern.compile(pattern);
+	}
+
+	public Map<String, Integer> getWordList() {
+		return wordList;
+	}
+	public void setWordList(Map<String, Integer> wordList) {
+		this.wordList = wordList;
+	}
 
 }
--- regex/src/main/java/com/venancio/dam/regex/Init.java
+++ regex/src/main/java/com/venancio/dam/regex/Init.java
@@ -1,8 +1,33 @@
 package com.venancio.dam.regex;
 
+import java.util.Scanner;
+
 public class Init {
 	public static void main(String[] args) {
-		System.out.println("Hola Equipo");
-		System.out.println("Hola!!!");
+		
+		
+		// Ejemplo de ejecución del ejercicio 6
+		System.out.println("-----------------------------------\nEJERCICIO 06: Ejemplo de ejecución:\n-----------------------------------");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Por favor, introduzca el patrón de separación de palabras: ");
+		String pattern = sc.nextLine();
+
+		System.out.print("Por favor, introduzca el número de repeticiones que ha de tener la palabra para que sea considerada término: ");
+		int rep = Integer.valueOf(sc.nextLine());
+		
+//		System.out.println("Por favor, introduzca o pegue el texto del que quiere extraer la terminología: ");
+//		StringBuilder text = new StringBuilder(sc.nextLine());
+		
+		sc.close();
+		
+		Ejercicio06 ej6 = new Ejercicio06(pattern);
+//		Ejercicio06 ej6b = new Ejercicio06(text, pattern);
+		
+//		System.out.println(ej6.wordExtraction());
+//		ej6.wordExtraction();
+		ej6.termExtraction(rep);
+
 	}
 }
