bdc7ec266b13efbe5d5972c971189f6a616f9f21
J. Martin S. <jennifer.marsor@educa.jcyl.es>
2022-04-11 14:13:47 +0200

    Ejercicio06 Class finished and Init for Ejercicio06 done

--- regex/src/main/java/com/venancio/dam/regex/Ejercicio06.java
+++ regex/src/main/java/com/venancio/dam/regex/Ejercicio06.java
@@ -11,14 +11,52 @@ public class Ejercicio06 {
 	private Pattern pattern;
 	private Map<String, Integer> wordList;
 	
+	private final String DEFAULT_SCI_TEXT = "2.1.1. Origen y funcionamiento de CRISPR-Cas.\r\n"
+			+ "\r\n"
+			+ "Como se ha especificado previamente, el sistema CRISPR es propio de las bacterias. Estos organismos lo utilizan como un sistema de defensa adaptativo frente a material genético (ADN) procedente del exterior, generalmente de virus bacteriófagos (un tipo de virus que infectan exclusivamente a las bacterias). Cuando este ADN exógeno entra en el interior de la bacteria, estas guardan una copia del mismo dentro de su propio genoma, como una forma de memoria de infecciones pasadas. Este ADN exógeno se integra en una región muy concreta del cromosoma bacteriano llamada locus CRISPR, el cual está formado por repeticiones cortas de ADN parcialmente palindrómico, alternadas con secuencias variables o espaciadores (Barrangou y Marraffini, 2014), de ahí su nombre (CRISPR, de Clustered Regularly Interspaced Short Palindromic Repeats). Dichas secuencias espaciadoras se corresponden con la copia del material genético que el virus bacteriófago introdujo en la bacteria. Cuando se produce una infección (introducción de ADN exógeno) por un virus del que ya existe información en el locus CRISPR (es decir, que ya infectó a la bacteria en el pasado), el sistema CRISPR actúa. Se producirán moléculas genéticas de ARN (una molécula que también alberga información, similar al ADN) usando como molde las secuencias espaciadoras del locus CRISPR (Zhao et al., 2014), que eran precisamente las copias del ADN del virus bacteriófago que la bacteria había guardado. Estas moléculas de ARN, guiarán a otras moléculas diferentes, las endonucleasas Cas (acrónimo de CRISPR-associated), hacia el ADN exógeno que el virus bacteriófago ha introducido, donde serán capaces de reconocer la secuencia genética igual a la que había en el locus CRISPR de la bacteria, a partir de la que fueron generadas. El complejo formado por la molécula de ARN y la endonucleasa Cas se unirá a esta secuencia y entonces actuará esta última, cuyo cometido será cortar la molécula de ADN (Mojica y Montoliu, 2016). De este modo, las bacterias consiguen destruir el material genético invasor, pues al cortar la molécula de ADN, la inutilizan.\r\n"
+			+ "\r\n"
+			+ "Gracias a las progresivas y constantes investigaciones llevadas a cabo por diferentes grupos de trabajo, se pudo dar con el funcionamiento de este sistema (detallado en el párrafo anterior) que permitió deducir que podría ser útil como instrumento de ayuda para modificar genes. Sin embargo, para poder utilizar CRISPRCas como una herramienta de edición génica, es necesario realizar algunas alteraciones en los elementos naturales que conforman el sistema bacteriano. Primeramente, existen tres tipos de sistemas CRISPR-Cas, cuyo funcionamiento, pese a ser en base el mismo, difiere en algunos puntos. El tipo de sistema CRISPR-Cas más utilizado como herramienta de edición génica es el tipo II, puesto que es a dicho grupo al que pertenece la endonucleasa más usada para este fin: la endonucleasa Cas9 (así pues, el sistema se denomina CRISPR-Cas9). Cuando actúa en las bacterias como estrategia defensiva, Cas9 necesita que haya dos moléculas de ARN unidas para guiarla hasta el ADN exógeno del virus bacteriófago y poder finalmente sesgarlo (Jiang y Doudna, 2015).\r\n"
+			+ "Para poder utilizar CRISPR-Cas9 como herramienta de edición génica, se ha de diseñar una única molécula de ARN guía, planteada como equivalente a la pareja de moléculas de ARN existente en los sistemas naturales de tipo II (Doudna y Charpentier, 2014).\r\n"
+			+ "\r\n"
+			+ "Por último, el resultado final de la acción de CRISPR-Cas sobre el ADN es un corte en su estructura. Queda de manifiesto, por tanto, que CRISPR-Cas modifica el genoma en tanto que genera un punto de ruptura, pero como para las células es inviable que la integridad del ADN esté comprometida, tienen mecanismos para reparar tal corte. Es decir, no basta solo con cortar el ADN; para que un gen esté propiamente modificado, y dicha modificación sea patente, el corte que el sistema CRISPR-Cas realiza debe ser reparado. Básicamente, la célula cuenta con dos estrategias diferentes para cumplir con esta tarea (ver figura 1):\r\n"
+			+ "\r\n"
+			+ "• una en la que, a partir de un molde de ADN, se realiza una copia de dicho molde en el hueco generado (Escribano-Díaz et al., 2013),\r\n"
+			+ "\r\n"
+			+ "• y otra en la que, ante la inexistencia de este molde genético, se insertan aleatoriamente nucleótidos (los elementos básicos de los que se compone el ADN), para rellenar el corte (Davis y Chen, 2013).\r\n"
+			+ "\r\n"
+			+ "Aunque en cada una de estas dos estrategias interviene un considerable número de diversos factores, la diferencia más importante es el resultado final de la reparación del corte.\r\n"
+			+ "\r\n"
+			+ "• En la primera estrategia, la información del gen en el que CRISPR-Cas ha producido el corte ha sido reemplazada por la información genética contenida en el molde de ADN a partir del cual la célula ha realizado la reparación.\r\n"
+			+ "\r\n"
+			+ "• En la segunda, en cambio, la secuencia que ha cortado CRISPR-Cas ha sido reparada mediante una inserción aleatoria de nucleótidos, lo que significa que se\r\n"
+			+ "ha sustituido por una combinación carente de información genética, por lo que el gen ha quedado inactivado.\r\n"
+			+ "\r\n"
+			+ "Esto es, en la primera estrategia el gen es reemplazado por otro (con una función diferente al original) y en la segunda estrategia el gen es inactivado (porque no se sustituye por ningún otro gen, sino por una combinación aleatoria de nucleótidos).\r\n"
+			+ "\r\n"
+			+ "Sabiendo esto, a la hora de plantear el procedimiento para modificar un gen concreto mediante el uso de CRISPR-Cas9, es preciso conocer la secuencia de nucleótidos del gen a modificar para así diseñar correctamente la molécula de ARN que guiará el corte que Cas9 realice (Brazelton et al., 2015). Además, si lo que se pretende es sustituir un gen por otro, se debe aportar también el molde de ADN con la información correspondiente al gen a incluir (Song y Stieger, 2017).\r\n"
+			+ "\r\n"
+			+ "Figura 1. Esquema del funcionamiento de las principales estrategias celulares de reparación de un corte en el ADN. Elaboración propia. Se ponen de manifiesto las principales diferencias entre ambas estrategias y su resultado final, reemplazamiento del gen o alelo en un caso e inactivación del mismo en el otro.\r\n"
+			+ "\r\n"
+			+ "1 Siglas para ácido desoxirribonucleico, una macromolécula biológica formada por una doble cadena de nucleótidos apareados.\r\n"
+			+ "2 Siglas para ácido ribonucleico, una macromolécula biológica formada por nucleótidos que, en la mayoría de los casos, se encuentra en forma de cadena sencilla, es decir, con los nucleótidos sin aparear.\r\n"
+			+ "3 Una endonucleasa es una proteína capaz de reconocer una secuencia concreta de nucleótidos en una molécula de ADN y cortar dicha molécula en el mismo punto de reconocimiento.\r\n"
+			+ "4 En realidad lo habitual no es reemplazar un gen por otro, sino reemplazar alelos. Se denominan alelos a todas aquellas posibles formas o variantes que puede tomar un mismo gen. Es decir, el gen sigue siendo el mismo pero cambia de variante, lo que conlleva, en la mayoría de las ocasiones, un cambio también de función. Para comprenderlo mejor, tomando un ejemplo mendeliano, el gen que regula el color de los guisantes puede tomar el alelo «A», y entonces el guisante será amarillo, o el alelo «a», y en cambio el guisante será verde; pero el gen «color» sigue siendo el mismo, por mucho que cambie su alelo.";
+	private final String DEFAULT_PATTERN = "[\s|.|,|:|;|(|)|[|]|{|}|¿|?|¡|!|\r\n]\s*";
+	
+	public Ejercicio06() {
+		this.sciText = new StringBuilder(DEFAULT_SCI_TEXT);
+		this.pattern = Pattern.compile(DEFAULT_PATTERN);
+		this.wordList = new HashMap<>();
+	}
+	
 	public Ejercicio06(String pat) {
-		this.sciText = new StringBuilder("texto de prueba, prueba, texto");
+		this.sciText = new StringBuilder(DEFAULT_SCI_TEXT);
 		this.pattern = Pattern.compile(pat);
 		this.wordList = new HashMap<>();
 	}
 	
-	public Ejercicio06(StringBuilder input, String pat) {
-		this.sciText = input;
+	public Ejercicio06(String input, String pat) {
+		this.sciText = new StringBuilder(input);
 		this.pattern = Pattern.compile(pat);
 		this.wordList = new HashMap<>();
 	}
@@ -33,10 +71,11 @@ public class Ejercicio06 {
 		Para ello recorremos el array con todas las palabras del texto y las vamos incluyendo en el mapa
 		de modo que la clave sea la palabra (String) como tal y el valor un entero (Integer) que iremos
 		incrementando a medida que se vaya repitiendo en el texto. */
-	    for(String word : wordByWord){
-	        if(!wordList.containsKey(word)){
+	    for (String word : wordByWord){
+	    	word = word.toLowerCase();
+	        if(!wordList.containsKey(word) && word != ""){
 	            wordList.put(word, 1);
-	        }else{
+	        } else if(word != "") {
 	            wordList.put(word, wordList.get(word)+1);
 	        }
 	    }
@@ -44,17 +83,18 @@ public class Ejercicio06 {
 	    return wordList;
 	}
 	
-	/* Método que, tras determinar e indicarle cuántas veces se ha de repetir una palabra para considerarla
-	término, extrae únicamente las palabras que aparecen en el texto más veces del número especificado */
-	public void termExtraction(int numOfTimes) {
+	/* Método que, tras determinar e indicarle el rango de cuántas veces se ha de repetir una palabra para
+	considerarla término, extrae únicamente las palabras que aparecen en el texto un número de veces dentro
+	de rango especificado */
+	public void termExtraction(Map<String, Integer> wordList, int minTimes, int maxTimes) {
 		
 		// Mediante un iterator, eliminamos del mapa las palabras con menos repeticiones de las indicadas
 		if(wordList.isEmpty()) {
 			System.out.println("ATENCIÓN: Aún no se ha extraído ninguna palabra del texto.\nPor favor, asegúrese de extraer todas las palabras del texto antes de realizar la extracción de la terminología.");
 		} else {
-			for(Iterator<Map.Entry<String, Integer>> it = wordList.entrySet().iterator(); it.hasNext();) {
+			for(Iterator<Map.Entry<String, Integer>> it = wordList.entrySet().iterator(); it.hasNext(); ) {
 				Map.Entry<String, Integer> word = it.next();
-				if(word.getValue() < numOfTimes) {
+				if(word.getValue() < minTimes || word.getValue() > maxTimes) {
 					it.remove();
 				}
 			}
@@ -79,7 +119,7 @@ public class Ejercicio06 {
 				
 //				// Otra opción
 //				for (Map.Entry<String, Integer> word : wordList.entrySet()) {
-//					System.out.print(word.getKey() + " se repite " + word.getValue() + " veces.");
+//					System.out.println(word.getKey() + "\t\t" + word.getValue());
 //				}
 			}
 					
--- regex/src/main/java/com/venancio/dam/regex/Init.java
+++ regex/src/main/java/com/venancio/dam/regex/Init.java
@@ -1,5 +1,6 @@
 package com.venancio.dam.regex;
 
+import java.util.Map;
 import java.util.Scanner;
 
 public class Init {
@@ -14,20 +15,27 @@ public class Init {
 		System.out.print("Por favor, introduzca el patrón de separación de palabras: ");
 		String pattern = sc.nextLine();
 
-		System.out.print("Por favor, introduzca el número de repeticiones que ha de tener la palabra para que sea considerada término: ");
-		int rep = Integer.valueOf(sc.nextLine());
+		System.out.print("Por favor, introduzca el número mínimo de repeticiones que ha de tener la palabra para que sea considerada término: ");
+		int min = Integer.valueOf(sc.nextLine());
+		System.out.print("Por favor, introduzca el número máximo de repeticiones que ha de tener la palabra para que sea considerada término: ");
+		int max = Integer.valueOf(sc.nextLine());
 		
 //		System.out.println("Por favor, introduzca o pegue el texto del que quiere extraer la terminología: ");
-//		StringBuilder text = new StringBuilder(sc.nextLine());
+//		String text = sc.nextLine();
 		
 		sc.close();
 		
-		Ejercicio06 ej6 = new Ejercicio06(pattern);
-//		Ejercicio06 ej6b = new Ejercicio06(text, pattern);
+//		Ejercicio06 ej6 = new Ejercicio06();
+		Ejercicio06 ej6A = new Ejercicio06(pattern);
+//		Ejercicio06 ej6B = new Ejercicio06(text, pattern);
 		
-//		System.out.println(ej6.wordExtraction());
-//		ej6.wordExtraction();
-		ej6.termExtraction(rep);
+//		Map<String, Integer> words = ej6.wordExtraction();
+//		System.out.println(words);
+		Map<String, Integer> wrds = ej6A.wordExtraction();
+//		System.out.println(wrds);
+		
+//		ej6.termExtraction(words, 3, 8);
+		ej6A.termExtraction(wrds, min, max);
 
 	}
 }
